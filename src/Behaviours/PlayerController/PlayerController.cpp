a6401c853b14e9c30e1fc279257843058fe9315d
Adise <[admin@pauibanez.me](mailto:admin@pauibanez.me)>
2026-05-22 14:27:10 +0200

    added level dependant ship sprites

--- src/Behaviours/PlayerController/PlayerController.cpp
+++ src/Behaviours/PlayerController/PlayerController.cpp
@@ -5,8 +5,10 @@
 #include <Crow2D/components/Animator.h>
 #include <Crow2D/components/Renderer.h>
 #include <Crow2D/dataObjects/AnimationClip.h>
+#include <Crow2D/dataObjects/Vectors.h>
 #include <algorithm>
 #include <stdexcept>
+#include <string>
 
 namespace Assteroids::Behaviours {
 using namespace Crow2D;
@@ -25,15 +27,36 @@ void PlayerController::Update() {
 
 
 void PlayerController::SetupVisuals() {
-  playerSprite = new Sprite("sprites/Player.png");
-  engineSprite = new Sprite("sprites/Engine.png");
-  engineEffects = new Sprite("sprites/EngineEffects.png");
-  engineClip = new AnimationClip(
-      engineEffects, Vector2(engineEffects->drawRect.w / 4.0f, engineEffects->drawRect.h));
+  LoadLevelSprites(1);
+  Vector2 size(1, 1);
+  engineRenderer = &gameObject->AddComponent<Renderer>(engineSprite, size);
+  shipRenderer = &gameObject->AddComponent<Renderer>(shipSprites[maxLives - 1], size);
+  GameObject &animatorGO = gameObject->CreateChild("Animations");
+  animatorGO.transform->position -= Vector3(0, 0, 1);
+  animatorGO.AddComponent<Renderer>(idleSprite, size);
+  animator = &animatorGO.AddComponent<Animator>(idleClip);
+}
 
-  gameObject->AddComponent<Renderer>(engineSprite, Vector2(1, 1));
-  gameObject->AddComponent<Renderer>(playerSprite, Vector2(1, 1));
-  animator = &gameObject->AddComponent<Animator>(engineClip);
+void PlayerController::LoadLevelSprites(short level) {
+  CleanSprites();
+
+
+  for (short i = 0; i < maxLives; i++) {
+    std::string name =
+        "sprites/ship/lvl" + std::to_string(level) + "/Ship_" + std::to_string(i) + ".png";
+
+    shipSprites.push_back(new Sprite(name.c_str()));
+  }
+
+  engineSprite = new Sprite(("sprites/ship/lvl" + std::to_string(level) + "/Engine.png").c_str());
+  idleSprite = new Sprite(("sprites/ship/lvl" + std::to_string(level) + "/Idle.png").c_str());
+  powerSprite = new Sprite(("sprites/ship/lvl" + std::to_string(level) + "/Power.png").c_str());
+
+  Vector2 idleCellSize(idleSprite->drawRect.w / 3.0f, idleSprite->drawRect.h);
+  idleClip = new AnimationClip(idleSprite, idleCellSize);
+
+  Vector2 powerCellSize(powerSprite->drawRect.w / 4.0f, powerSprite->drawRect.h);
+  powerClip = new AnimationClip(powerSprite, powerCellSize);
 }
 
 void PlayerController::MovePlayer() {
@@ -41,10 +64,10 @@ void PlayerController::MovePlayer() {
 
   if (InputManager::GetKey("W").isPressed) {
     currentSpeed = std::clamp(currentSpeed + acceleration * Time::deltaTime, 0.0f, maxSpeed);
-    // if (animator->isPaused) animator->Resume();
+    if (animator->currentClip != powerClip) animator->Play(powerClip);
   } else if (currentSpeed > 0) {
     currentSpeed = std::clamp(currentSpeed - acceleration * Time::deltaTime, 0.0f, maxSpeed);
-    // if (!animator->isPaused) animator->Pause();
+    if (animator->currentClip != idleClip) animator->Play(idleClip);
   }
 
   transform->Translate(Vector3(transform->forward) * currentSpeed * Time::deltaTime);
@@ -52,12 +75,22 @@ void PlayerController::MovePlayer() {
 }
 
 
-void PlayerController::OnDestroy() {
-  delete playerSprite;
-  delete engineSprite;
-  delete engineEffects;
-  delete engineClip;
+void PlayerController::CleanSprites() {
+  if (!shipSprites.empty()) {
+    for (Sprite *shipSprite : shipSprites) {
+      delete shipSprite;
+    }
+  }
+  shipSprites.clear();
+  if (engineSprite) delete engineSprite;
+  if (idleSprite) delete idleSprite;
+  if (powerSprite) delete powerSprite;
+
+  if (idleClip) delete idleClip;
+  if (powerClip) delete powerClip;
 }
+
+void PlayerController::OnDestroy() { CleanSprites(); }
 
 
 } // namespace Assteroids::Behaviours
--- src/Behaviours/PlayerController/PlayerController.h
+++ src/Behaviours/PlayerController/PlayerController.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <Crow2D/Component.h>
 #include <Crow2D/Crow2D.h>
 #include <Crow2D/components/Animator.h>
+#include <Crow2D/components/Renderer.h>
 #include <Crow2D/dataObjects/AnimationClip.h>
 #include <Crow2D/dataObjects/Sprite.h>
+#include <vector>
 
 
 namespace Assteroids::Behaviours {
@@ -15,17 +18,24 @@ public:
   float acceleration = 2;
   float maxSpeed = 3;
   float turnSpeed = 45;
+  short maxLives = 4;
+  short currentLives = maxLives;
 
 private:
   float currentSpeed = 0;
 
   Crow2D::Components::Animator *animator;
+  Crow2D::Components::Renderer *shipRenderer;
+  Crow2D::Components::Renderer *engineRenderer;
 
 private:
-  Crow2D::Types::Sprite *playerSprite = nullptr;
+  std::vector<Crow2D::Types::Sprite *> shipSprites;
   Crow2D::Types::Sprite *engineSprite = nullptr;
-  Crow2D::Types::Sprite *engineEffects = nullptr;
-  Crow2D::Animations::AnimationClip *engineClip = nullptr;
+  Crow2D::Types::Sprite *idleSprite = nullptr;
+  Crow2D::Types::Sprite *powerSprite = nullptr;
+
+  Crow2D::Animations::AnimationClip *idleClip = nullptr;
+  Crow2D::Animations::AnimationClip *powerClip = nullptr;
   // #endregion
 
   // #region Crow2D
@@ -38,9 +48,13 @@ private:
   // #region Methods
 private:
   void SetupVisuals();
+  void LoadLevelSprites(short level);
+
   void MovePlayer();
   void RotatePlayer();
   void Shoot();
+
+  void CleanSprites();
   // #endregion
 };
 } // namespace Assteroids::Behaviours
