19bf5d1cef5dbda02a4bb16f078b1603a68eda75
Adise <[admin@pauibanez.me](mailto:admin@pauibanez.me)>
2026-05-23 18:29:03 +0200

    wired the Ui with the data

--- assets/ui/scripts/HUD/HUD.js
+++ assets/ui/scripts/HUD/HUD.js
@@ -1,12 +1,19 @@
 "use strict";
 const points = document.querySelector("#points");
-const stats = document.querySelector("#stats");
+const statsHolder = document.querySelector(".statHolder");
+let stats;
 const UpdatePoints = (newPoints) => {
     points.textContent = newPoints;
 };
 const DisplayStats = (statValues) => {
-    console.log("Got stats!");
-    stats.textContent = statValues;
+    stats = JSON.parse(statValues);
+    stats.forEach((stat) => {
+        const value = `<div class="stat" id="${stat.name}">
+          <p class="statName">${stat.name}</p>
+          <p class="statValue">${stat.value * 10}</p>
+        </div>`;
+        statsHolder.innerHTML += value;
+    });
 };
 window.bridge.on("Points", UpdatePoints);
 window.bridge.on("Stats", DisplayStats);
--- ts/HUD/HUD.ts
+++ ts/HUD/HUD.ts
@@ -1,13 +1,28 @@
 const points = document.querySelector("#points");
-const stats = document.querySelector("#stats");
+const statsHolder = document.querySelector(".statHolder");
+
+interface Stat {
+  name: string;
+  value: number;
+  level: number;
+}
+
+let stats: Stat[];
 
 const UpdatePoints = (newPoints?: string | undefined) => {
   points!.textContent = newPoints!;
 };
 
 const DisplayStats = (statValues?: string | undefined) => {
-  console.log("Got stats!");
-  stats!.textContent = statValues!;
+  stats = JSON.parse(statValues!);
+  stats.forEach((stat) => {
+    const value = `<div class="stat" id="${stat.name}">
+          <p class="statName">${stat.name}</p>
+          <p class="statValue">${stat.value * 10}</p>
+        </div>`;
+
+    statsHolder!.innerHTML += value;
+  });
 };
 
 window.bridge.on("Points", UpdatePoints);
